e4bce97a0eec870f9485e858ac5bfd072c5c88e9
sabuj.das <sabuj.das@ce625cfe-8a2e-bfbe-4165-a08eaf51a503>
2012-11-07 14:27:24 +0000

    EXE created... by launch4j

---
+++ trunk/dbex/dbex-design/src/main/java/com/gs/dbex/design/ui/DatabaseDesignerCanvas.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.gs.dbex.design.ui;
+
+/**
+ * @author Sabuj Das | sabuj.das@gmail.com
+ *
+ */
+public class DatabaseDesignerCanvas {
+
+	/**
+	 * 
+	 */
+	public DatabaseDesignerCanvas() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
